3be1a3d3669d16f142ceadb929a714ed5e44d8f8
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-05-22 15:58:41 +0200

    feat: added more complex validation

--- partials/functions.php
+++ partials/functions.php
@@ -1,6 +1,9 @@
 <?php
 
-function validator($email){
-    return str_contains($email, "@") && str_contains($email, ".") ? true : false;
+// function validator($email){
+//     return str_contains($email, "@") && str_contains($email, ".") ? true : false;
 
+// }
+function validator($email){
+    return preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $email) ? true : false;
 }
